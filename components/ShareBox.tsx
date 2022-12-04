ad816c5fb9a9b68b56e04f9bfabb99adedfdb499
hafizhmaulanay <56567633+hafizhmaulanay@users.noreply.github.com>
2022-12-04 21:56:24 +0700

    fix(sharebox): :bug: fix sharebox pop up

--- components/ShareBox.tsx
+++ components/ShareBox.tsx
@@ -33,61 +33,56 @@ export default function ShareArticle({ text, url }: { text: string; url: string 
           Sharing is caring. It helps me to grow and improve my content.
         </p>
       </div>
-      <button
-        className="mt-4 flex h-8 w-full items-center justify-center rounded bg-gray-100 px-4 py-1 font-medium text-gray-900 dark:bg-gray-700 dark:text-gray-100 md:w-28"
-        type="submit"
-      >
-        {isSupportWebShare ? (
-          <a
-            href={''}
-            onClick={shareMe}
-            title="Share this article"
-            className="flex items-center justify-center"
+      {isSupportWebShare ? (
+        <a
+          href={''}
+          onClick={shareMe}
+          title="Share this article"
+          className="mt-4 flex h-8 w-full items-center justify-center rounded bg-gray-100 px-4 py-1 font-medium text-gray-900 dark:bg-gray-700 dark:text-gray-100 md:w-28"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="mr-2 h-4 w-4"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-4 w-4"
-            >
-              <line x1="22" y1="2" x2="11" y2="13" />
-              <polygon points="22 2 15 22 11 13 2 9 22 2" />
-            </svg>
-            Share
-          </a>
-        ) : (
-          <a
-            href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
-              text + ' - by @hafizhmaulanay'
-            )}&url=${url}`}
-            title="Share to Twitter"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center justify-center "
+            <line x1="22" y1="2" x2="11" y2="13" />
+            <polygon points="22 2 15 22 11 13 2 9 22 2" />
+          </svg>
+          Share
+        </a>
+      ) : (
+        <a
+          href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+            text + ' - by @hafizhmaulanay'
+          )}&url=${url}`}
+          title="Share to Twitter"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mt-4 flex h-8 w-full items-center justify-center rounded bg-gray-100 px-4 py-1 font-medium text-gray-900 dark:bg-gray-700 dark:text-gray-100 md:w-28"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="mr-2 h-4 w-4"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2"
-            >
-              <line x1="22" y1="2" x2="11" y2="13" />
-              <polygon points="22 2 15 22 11 13 2 9 22 2" />
-            </svg>
-            Share
-          </a>
-        )}
-      </button>
+            <line x1="22" y1="2" x2="11" y2="13" />
+            <polygon points="22 2 15 22 11 13 2 9 22 2" />
+          </svg>
+          Share
+        </a>
+      )}
     </div>
   );
 }
