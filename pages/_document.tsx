fb29942b7edf23d92f4c6844551d54ca79ac8275
hafizhmaulanay <56567633+hafizhmaulanay@users.noreply.github.com>
2022-12-05 18:44:55 +0700

    chore(PWA): :zap: improve PWA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,8 +10,8 @@ export default function Document() {
         <link href="/static/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
         <link href="/static/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
         <link color="#4a9885" href="/static/favicons/safari-pinned-tab.svg" rel="mask-icon" />
-        <meta content="#ffffff" name="theme-color" />
-        <meta content="#ffffff" name="msapplication-TileColor" />
+        <meta content="#000000" name="theme-color" />
+        <meta content="#000000" name="msapplication-TileColor" />
         <meta content="/static/favicons/browserconfig.xml" name="msapplication-config" />
         <meta
           content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
