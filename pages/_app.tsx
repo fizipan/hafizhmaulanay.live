726ef507fd87ae06553b648939adbd5eb985a8bb
hafizhmaulanay <56567633+hafizhmaulanay@users.noreply.github.com>
2022-12-07 10:04:55 +0700

    perf(analytic): :chart_with_upwards_trend: add google analytic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app';
 import { Inter } from '@next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
 
 const interVariable = Inter();
 
@@ -11,6 +12,20 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <main className={interVariable.className}>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-96CZSLKGHT"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-96CZSLKGHT');
+        `}
+        </Script>
         <Component {...pageProps} />
         <Analytics />
       </main>
