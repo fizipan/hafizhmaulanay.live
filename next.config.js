4926aa3ce6658378e3fac02032321f457a6d283b
fizipan <hafizhmy26@gmail.com>
2026-06-26 13:54:21 +0700

    feat: modernize stack and redesign contact page

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
-  },
+  experimental: {},
   async headers() {
     return [
       {
